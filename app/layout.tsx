1abc7736a418c2accc4d1bb1e5ed280c454a4908
Kumar Sunil Verma <kumarsunilverma@Durgas-Macbook-Pro.local>
2023-04-03 16:16:32 +0530

    name update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ import ClientOnly from './components/ClientOnly';
 import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
+  title: 'FDP INDIA',
+  description: 'FDP INDIA',
 }
 
 const font = Nunito({ 
