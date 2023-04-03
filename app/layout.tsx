b6ea6b9cd29b3e9806412d97583cef0fcd9bfd58
Kumar Sunil Verma <kumarsunilverma@Durgas-Macbook-Pro.local>
2023-04-03 17:09:46 +0530

    test name 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import ClientOnly from './components/ClientOnly';
 import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
-  title: 'FDP INDIA',
+  title: 'FDP INDIA update',
   description: 'FDP INDIA',
 }
 
