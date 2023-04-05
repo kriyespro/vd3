5cf8c9d200d31e4459b59e75bf8d0f3fb048da97
Kumar Sunil Verma <kumarsunilverma@Durgas-Macbook-Pro.local>
2023-04-05 17:06:35 +0530

    icon update 123

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -118,10 +118,10 @@ const ListingCard: React.FC<ListingCardProps> = ({
         </div>
         <div className="flex flex-row items-center gap-1">
           <div className="font-semibold">
-            $ {price}
+            INR: {price}
           </div>
           {!reservation && (
-            <div className="font-light">night</div>
+            <div className="font-light">Sq Ft</div>
           )}
         </div>
         {onAction && actionLabel && (
