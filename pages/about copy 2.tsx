6605ce9d579e66e17601a6b03a4001653a19878a
Kumar Sunil Verma <kumarsunilverma@Durgas-Macbook-Pro.local>
2023-04-18 18:34:22 +0530

    tw

--- pages/about copy 2.tsx
+++
@@ -1,5 +0,0 @@
-export default function About(){
-    return  <div className="text-center text-3xl font-bold"> About us </div>
-    
-    
-}
--- pages/rp.tsx
+++ pages/rp.tsx
@@ -1,20 +1,20 @@
-// export default function Rp() {
-//     return (
-//       <h1 className="text-3xl font-bold underline">
-//         Hello world!
-//       </h1>
-//     )
-//   }
+export default function Rp() {
+    return (
+      <h1 className="text-3xl text-blue-500 font-bold underline">
+        Hello world!
+      </h1>
+    )
+  }
 
 
-import React from 'react'
+// import React from 'react'
   
-const HomePage = () => {
-  return (
-    <div className='text-3xl text-green-600 p-2'>
-      Hello Geeks!
-    </div>
-  )
-}
+// const HomePage = () => {
+//   return (
+//     <div className='text-3xl text-green-600 p-2'>
+//       Hello Geeks!
+//     </div>
+//   )
+// }
   
-export default HomePage
+// export default HomePage
